16fdabe4c99a9dbad1a6be02ffd87668e5f27267
eloutassi salma <salma03abb@gmail.com>
2024-05-10 23:56:37 +0100

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+
+
+const AppLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name ="index" options={{ headerShown: false }}/>
+        <Stack.Screen name ="home" options={{ headerShown: false }}/>
+    </Stack>
+  )
+}
+
+export default AppLayout
---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
+
+const App = () => {
+  
+
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
